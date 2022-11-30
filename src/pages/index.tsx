9e9e0655fc21b61b171200ca9a8c026e90e7d7a7
Caio Cesar <caiolopesbarros@hotmail.com>
2022-11-30 20:17:54 -0300

    toni lindo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,71 @@ import Head from 'next/head'
 import { Header, HouseCard } from '../components'
 
 export default function Home() {
+  const imoveis = [
+    {
+      nome: 'Casa do Seu José',
+      cidade: 'Poços de Caldas - MG',
+      codigo: 154,
+      n_quartos: 2,
+      n_carros: 1,
+      valor: 9000,
+      imagem:
+        'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    },
+    {
+      nome: 'Triplex aconchegante',
+      cidade: 'São João da Boa Vista - SP',
+      codigo: 133,
+      n_quartos: 9,
+      n_carros: 3,
+      valor: 10000,
+      imagem:
+        'https://www.revistause.com.br/wp-content/uploads/2022/09/villandry-1-1000x600.jpg',
+    },
+    {
+      nome: 'Chalé Rústico',
+      cidade: 'Poços de Caldas - MG',
+      codigo: 295,
+      n_quartos: 5,
+      n_carros: 1,
+      valor: 4000,
+      imagem:
+        'https://i.pinimg.com/236x/bb/84/17/bb84179759a6dfd5df48166fee4989f2.jpg',
+    },
+    {
+      nome: 'Luxuosa Casa de Férias',
+      cidade: 'Divinolândia - SP',
+      codigo: 51,
+      n_quartos: 14,
+      n_carros: 5,
+      valor: 15000,
+      imagem:
+        'https://media.vrbo.com/lodging/35000000/34630000/34625200/34625104/f1c847c7.f6.jpg',
+    }
+  ];
+
+  const imoveis_usuario = [
+    {
+      nome: 'Casa no centro',
+      cidade: 'Poços de Caldas - MG',
+      codigo: 19,
+      n_quartos: 4,
+      n_carros: 1,
+      valor: 1200,
+      imagem: 'https://mapio.net/images-immo2-detalhe/17467146/-img-0.jpeg',
+    },
+    {
+      nome: 'Casa de Campo dos Martins',
+      cidade: 'Poços de Caldas - MG',
+      codigo: 301,
+      n_quartos: 6,
+      n_carros: 4,
+      valor: 2000,
+      imagem:
+        'https://cf.bstatic.com/xdata/images/hotel/max500/346144301.jpg?k=10d526b5542b7357eff4a754d6d67bc2fc3cec22b95f835d691ba518c60fbc0e&o=&hp=1',
+    },
+  ];
+
   return (
     <>
       <Head>
--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -4,6 +4,122 @@ export default function Perfil() {
   return (
     <>
       <Header />
+      <main>
+        
+        <div className="container mx-auto">
+          <div className="grid mt-8 lg:grid-cols-2 lg:gap-4">
+            <div className="flex items-center  gap-4 p-2 rounded border border-zinc-200">
+              <div>
+                <h1>Imóveis Alugados</h1>
+              </div>
+              <div>
+                <table>
+                  <tr>
+                    <th>Imóvel</th>
+                    <th>Proprietário</th>
+                    <th>E-mail</th>
+                    <th>Status da Mensalidade</th>
+                    <th></th>
+                  </tr>
+                  <tr>
+                    <td>Apartamento em São Paulo</td>
+                    <td>Maicon Dias</td>
+                    <td>maicon@email</td>
+                    <td>Paga</td>
+                    <td>
+                      {/* se clicar, aparece um modal com o qrcode no pix, algo assim */}
+                      <button>
+                        Botão - Pagar Aluguel
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Sala Comercial - Poços Premium</td>
+                    <td>Jerry Smith</td>
+                    <td>jerry@email</td>
+                    <td>Atrasada</td>
+                    <td>
+                      <button>
+                      Botão - Pagar Aluguel
+                      </button>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="grid mt-8 lg:grid-cols-2 lg:gap-4">
+            <div className="flex items-center  gap-4 p-2 rounded border border-zinc-200">
+              <div>
+                <h1>Propostas recebidas</h1>
+              </div>
+              <div>
+                <table>
+                  <tr>
+                    <th>Imóvel</th>
+                    <th>Proponente</th>
+                    <th>E-mail</th>
+                    <th>CPF</th>
+                    <th>Telefone</th>
+                    <th>Tipo</th>
+                    <th>Oferta</th>
+                  </tr>
+                  <tr>
+                    <td>Casa de campo dos Martins</td>
+                    <td>Caio Cesar Pan Lopes de Barros</td>
+                    <td>caiocesar@email</td>
+                    <td>123.123.123-12</td>
+                    <td>35 94234-4536</td>
+                    <td>Compra</td>
+                    <td>Olá, Jean. Tudo bem? Tenho interesse em comprar este imóvel, gostaria de agendar uma visita! Muito obrigado</td>
+                  </tr>
+                  <tr>
+                    <td>Apartamento Luxuoso</td>
+                    <td>Ulysses Machado</td>
+                    <td>Ulysses@email</td>
+                    <td>123.123.123-12</td>
+                    <td>35 94234-4536</td>
+                    <td>Aluguel</td>
+                    <td>E aí, Jean. Tranquilo? Vou estar em Poços durante janeiro e fevereiro e gostaria de alugar seu imóvel durante esse período</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="grid mt-8 lg:grid-cols-2 lg:gap-4">
+            <div className="flex items-center  gap-4 p-2 rounded border border-zinc-200">
+              <div>
+                <h1>Propostas enviadas</h1>
+              </div>
+              <div>
+                <table>
+                  <tr>
+                    <th>Imóvel</th>
+                    <th>Proprietário</th>
+                    <th>E-mail</th>
+                    <th>Tipo</th>
+                    <th>Oferta</th>
+                  </tr>
+                  <tr>
+                    <td>Chalé Rústico e aconchegante</td>
+                    <td>Lucas Paiva</td>
+                    <td>LukinhasFera@email</td>
+                    <td>Aluguel</td>
+                    <td>Boa noite! Gostaria de alugar o chalé para minha Lua de Mel</td>
+                  </tr>
+                  <tr>
+                    <td>Sala comercial no centro</td>
+                    <td>Ulysses Machado</td>
+                    <td>Ulysses@email</td>
+                    <td>Aluguel</td>
+                    <td>Boa dia! Tenho interesse em alugar esta sala comercial por 3 anos</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
